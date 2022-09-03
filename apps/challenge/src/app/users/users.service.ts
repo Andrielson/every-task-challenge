dd9c236f96da3b5e65e2ed2ad3e37f9ddb6d5ffb
Andrielson Silva <andrielson@gmail.com>
2022-09-03 13:05:37 -0300

    Initial implementation of UsersService

--- apps/challenge/src/app/users/users.service.ts
+++ apps/challenge/src/app/users/users.service.ts
@@ -1,4 +1,28 @@
+import { randomUUID } from 'crypto';
 import { Injectable } from '@nestjs/common';
+import { User } from './user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: randomUUID(),
+      email: 'user_one@email.com',
+      password: 'password-one',
+    },
+    {
+      id: randomUUID(),
+      email: 'user_two@email.com',
+      password: 'password-two',
+    },
+    {
+      id: randomUUID(),
+      email: 'user_three@email.com',
+      password: 'password-three',
+    },
+  ];
+
+  findOne(email: string): User | undefined {
+    return this.users.find(user => user.email === email);
+  }
+}
