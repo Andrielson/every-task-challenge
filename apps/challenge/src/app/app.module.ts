8523dcda80eb56d04f52e608ff42489f2c54f528
Andrielson Silva <andrielson@gmail.com>
2022-09-03 13:19:32 -0300

    nx generate @nrwl/nest:module crypto --project challenge --directory app

--- apps/challenge/src/app/app.module.ts
+++ apps/challenge/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, CryptoModule],
   controllers: [AppController],
   providers: [AppService],
 })
