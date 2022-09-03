dc2e4f2924d330a6267e427829743ffcec3acbe1
Andrielson Silva <andrielson@gmail.com>
2022-09-03 15:03:37 -0300

    Authentication implemented

--- apps/challenge/src/app/app.service.ts
+++ apps/challenge/src/app/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from './users/users.service';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to challenge!' };
+  constructor(private readonly usersService: UsersService) {}
+
+  getData() {
+    return this.usersService.findOne('user_one@email.com');
   }
 }
--- apps/challenge/src/app/auth/auth.controller.ts
+++ apps/challenge/src/app/auth/auth.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { User } from '../users/user.interface';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Req() req: Request) {
     return this.authService.login(req.user as User);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Req() req: Request) {
+    return req.user;
   }
 }
