56baf4adfcec727636d83de5de9232bfe0bfe087
Andrielson Silva <andrielson@gmail.com>
2022-09-03 18:06:17 -0300

    Increase code coverage for CryptoService

--- apps/challenge/src/app/crypto/crypto.service.spec.ts
+++ apps/challenge/src/app/crypto/crypto.service.spec.ts
@@ -15,4 +15,19 @@ describe('CryptoService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should encode password', async () => {
+    const password = 'password';
+    const encodePassword = await service.encodePassword(password);
+    expect(encodePassword).toBeTruthy();
+    expect(encodePassword).not.toBe(password);
+    expect(encodePassword.length).toBeGreaterThan(password.length);
+  });
+
+  it('should verify password and hash', async () => {
+    const password = 'password';
+    const encodePassword = await service.encodePassword(password);
+    const isEqual = await service.verifyPassword(password, encodePassword);
+    expect(isEqual).toBe(true);
+  });
 });
