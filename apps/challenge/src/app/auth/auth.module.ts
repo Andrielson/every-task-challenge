56b6994bbb1ff3e116e49055ddf5861a6ecdb9bd
Andrielson Silva <andrielson@gmail.com>
2022-09-03 13:56:56 -0300

    Add passport local strategy

--- apps/challenge/src/app/auth/auth.module.ts
+++ apps/challenge/src/app/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { CryptoModule } from '../crypto/crypto.module';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [CryptoModule, UsersModule],
-  providers: [AuthService],
+  imports: [CryptoModule, UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
