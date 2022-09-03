c5318ec87cac3b94a3bdddeab802f8749080379c
Andrielson Silva <andrielson@gmail.com>
2022-09-03 18:22:43 -0300

    Increase code coverage for UsersService

--- apps/challenge/src/app/users/users.service.spec.ts
+++ apps/challenge/src/app/users/users.service.spec.ts
@@ -1,19 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CryptoService } from '../crypto/crypto.service';
 import { UsersService } from './users.service';
+import USERS from './users';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let cryptoService: CryptoService;
+  let userService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CryptoService, UsersService],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    cryptoService = module.get<CryptoService>(CryptoService);
+    userService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  it('should find one existing user by email', async () => {
+    const existingEmail = USERS[2].email;
+    jest
+      .spyOn(cryptoService, 'encodePassword')
+      .mockImplementation((value) => Promise.resolve(value));
+    await userService.onModuleInit();
+
+    const user = userService.findOne(existingEmail);
+
+    expect(user).toEqual(USERS[2]);
+  });
+
+  it('should return null if does not find a user with supplied email', () => {
+    const loggerSpy = jest.spyOn(userService['logger'], 'log');
+    const email = 'some@email.com';
+
+    const user = userService.findOne(email);
+
+    expect(user).not.toBeTruthy();
+    expect(loggerSpy).toHaveBeenCalledWith(`User not found: ${email}!`);
   });
 });
