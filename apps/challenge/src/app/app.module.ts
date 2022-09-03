4804d662b5ff018384487102417a8bc10eb9e24f
Andrielson Silva <andrielson@gmail.com>
2022-09-03 16:51:39 -0300

    Remove boilerplate

--- apps/challenge/src/app/app.module.ts
+++ apps/challenge/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CryptoModule } from './crypto/crypto.module';
@@ -9,7 +7,5 @@ import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [AuthModule, UsersModule, CryptoModule, TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
