1980a79dffb9d54e64c9c3b020f47b21e3420d8c
Andrielson Silva <andrielson@gmail.com>
2022-09-03 13:44:30 -0300

    Implementation of CryptoService

--- apps/challenge/src/app/crypto/crypto.service.ts
+++ apps/challenge/src/app/crypto/crypto.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { randomFillSync } from 'crypto';
+import { Injectable, Logger } from '@nestjs/common';
+import { bcrypt, bcryptVerify } from 'hash-wasm';
 
 @Injectable()
-export class CryptoService {}
+export class CryptoService {
+  private readonly logger = new Logger(CryptoService.name);
+
+  async encodePassword(password: string) {
+    const salt = new Uint8Array(16);
+    randomFillSync(salt);
+
+    try {
+      return await bcrypt({
+        password,
+        salt,
+        costFactor: 11,
+      });
+    } catch (error) {
+      this.logger.error(
+        'An error was throwed while encoding password: ',
+        error
+      );
+      throw new Error('Password encryption exception');
+    }
+  }
+
+  async verifyPassword(password: string, hash: string) {
+    try {
+      return await bcryptVerify({ password, hash });
+    } catch (error) {
+      this.logger.error(
+        'An error was throwed while verifying password: ',
+        error
+      );
+      throw new Error('Password verify exception');
+    }
+  }
+}
